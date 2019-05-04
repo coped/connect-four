682323a38b8abf82b3c9e2f1e98171aaf02dd953
coped <dennisaaroncope@gmail.com>
2019-05-04 16:32:19 -0700

    Add classes and class methods necessary to build game board, coordinate system, and token system.

--- lib/main.rb
+++ lib/main.rb
@@ -1,22 +1,84 @@
+class TokenNode
+    attr_accessor :position, :value
+
+    def initialize(position, value = " ")
+        @position = position
+        @value = value
+    end
+end
+
 class Player
-    def initialize symbol
+    attr_reader :symbol
+
+    def initialize(symbol)
         @symbol = symbol
     end
 end
 
 class GameBoard
+    attr_reader :coordinates, :board
+
     def initialize
+        @coordinates = create_coordinates
+        @board = create_board
         display_board
     end
-
+    
     def display_board
-        puts "BOARD"
+        puts ""
+        print "|"
+        @board[35..41].each { |token| print " #{token.value} |" }
+        print "\n"
+        print "|"
+        @board[28..34].each { |token| print " #{token.value} |" }
+        print "\n"
+        print "|"
+        @board[21..27].each { |token| print " #{token.value} |" }
+        print "\n"
+        print "|"
+        @board[14..20].each { |token| print " #{token.value} |" }
+        print "\n"
+        print "|"
+        @board[7..13].each { |token| print " #{token.value} |" }
+        print "\n"
+        print "|"
+        @board[0..6].each { |token| print " #{token.value} |" }
+        print "\n"
+        puts ""
+        puts "  1   2   3   4   5   6   7"
+    end
+
+    def make_turn
+        input = gets.chomp.to_i
+        @coordinates.each
+    end
+
+    def create_coordinates
+        coordinates = []
+        6.times do |index|
+            column_index = index
+            7.times do |index|
+                position = []
+                position << column_index
+                position.unshift(index)
+                coordinates << position
+            end
+        end
+        coordinates
+    end
+
+    def create_board
+        board = []
+        @coordinates.each do |coordinate|
+            token = TokenNode.new(coordinate)
+            board << token
+        end
+        board
     end
 end
 
-def main
+def play_game
     game = GameBoard.new
-    game.display_board
 end
 
-main
+play_game
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,11 +1,25 @@
 require './lib/main'
 
+describe TokenNode do
+    context "when initialized" do
+        let(:empty_token) { TokenNode.new([0, 0]) }
+        let(:played_token) { TokenNode.new([0, 0], "X") }
+            
+        it "has a default value of \" \"" do
+            expect(empty_token.value).to eql(" ")
+        end
+        it "takes and stores a provided value" do
+            expect(played_token.value).to eql("X")
+        end
+    end
+end
+
 describe Player do
-    subject {Player.new "X"}
+    subject { Player.new "X" }
 
     describe "#initialize" do
         context "when initializing player object" do
-            it {is_expected.not_to be_nil}
+            it { is_expected.not_to be_nil }
         end
     end
 end
@@ -15,18 +29,32 @@ describe GameBoard do
 
     describe "#initialize" do
         context "when initializing GameBoard object" do
-            it {is_expected.not_to be_nil}
+            it { is_expected.not_to be_nil }
         end
     end
     describe "#display_board" do
         context "when calling #display_board between turns" do
-            it "should display game board" do
+            it "displays game board" do
                 expect(subject).to receive(:display_board)
                 subject.display_board
             end
         end
     end
+    describe "#create_coordinates" do
+        it "creates an array of 42 (7x6) coordinates" do
+            size = 0
+            subject.create_coordinates.each { size += 1 }
+            expect(size).to eql(42)
+        end
+    end
+    describe "#create_board" do
+        it "creates an array of nodes for each board coordinate" do
+            size = 0
+            subject.create_board.each { size += 1}
+            expect(size).to eql(42)
+        end
+    end
 end
 
-describe "#main" do
+describe "#play_game" do
 end
